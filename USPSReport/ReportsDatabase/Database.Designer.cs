6852c640936353ef24253148bd90027f4f1c2bbd
MaheswaraRaoK <55400944+MaheswaraRaoK@users.noreply.github.com>
2021-04-22 13:31:52 -0400

    Latest updates of schema.

--- USPSReport/ReportsDatabase/Database.Designer.cs
+++ USPSReport/ReportsDatabase/Database.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\FinalProject\JBIntranet\USPSReport\ReportsDatabase\Database.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\maheshkattamuribpl\Documents\MaheswaraRaoK\Workspace\GitHub\JBIntranet-New\USPSReport\ReportsDatabase\Database.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
