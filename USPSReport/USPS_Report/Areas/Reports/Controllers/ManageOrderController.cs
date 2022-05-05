8b6fff4e7d580b4a027ef1874c48668960325fff
MaheswaraRaoK <55400944+MaheswaraRaoK@users.noreply.github.com>
2022-05-05 14:33:03 -0400

    #54-03282022 changes.

--- USPSReport/USPS_Report/Areas/Reports/Controllers/ManageOrderController.cs
+++ USPSReport/USPS_Report/Areas/Reports/Controllers/ManageOrderController.cs
@@ -8,12 +8,13 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static USPS_Report.Areas.Reports.Models.ManageOrder;
 
 namespace USPS_Report.Areas.Reports.Controllers
 {
     public class ManageOrderController : Controller
     {
-       
+
         string dir = @"\\jbmwix-azfs01\IT\IntranetDocuments\StateAudit$\Files\";
         // GET: Reports/ManageOrders
         public ActionResult CancelOrders()
@@ -21,14 +22,14 @@ namespace USPS_Report.Areas.Reports.Controllers
             return View();
         }
 
-        
+
 
         [HttpPost]
-        public ActionResult CancelOrders(HttpPostedFileBase file,string Note)
+        public ActionResult CancelOrders(HttpPostedFileBase file, string Note)
         {
             try
             {
- 
+
                 // Check validations
                 HttpFileCollectionBase files = Request.Files;
                 var errorMsg = "";
@@ -75,22 +76,48 @@ namespace USPS_Report.Areas.Reports.Controllers
                                 list.Add(Convert.ToInt32(row.Cell(1).Value.ToString()));
                             }
                         }
+                        var cancelList = new List<Int32>();
+                        List<ExcludedOrder> excludeList = new List<ExcludedOrder>();
+                        //var excludeList = new List<Int32>();
+                        using (HHSQLDBEntities _db = new HHSQLDBEntities())
+                        {
+                            IList<tbl_PS_WorkOrder> _woListTemp = new List<tbl_PS_WorkOrder>();
+                            _woListTemp = _db.tbl_PS_WorkOrder.Where(t => list.Contains(t.ID)).OrderByDescending(t => t.ID).ToList();
+
+                            foreach (tbl_PS_WorkOrder item in _woListTemp)
+                            {
+                                string Status = item.Cancel_Date != null ? "Cancelled" : item.Completed_Date != null ? "Completed" : item.LastPrintDate != null ? "Printed/Sent to oracle" :
+                                                (item.HoldFromShipping == 1 && item.HoldFromShippingReason == null) ? "Created" :
+                                                (item.HoldFromShipping == 1 && item.HoldFromShippingReason != null) ? "Holding" :
+                                                (item.HoldFromShipping == 1 && item.HoldFromShippingReason.Contains("%Back Order%")) ? "Back Ordered and Holding" :
+                                                (item.HoldFromShipping == 1 && item.HoldFromShippingReason.Contains("Back Order ~")) ? "Back Ordered" : "Waiting to Interface";
+                                //string Status = "Printed/Sent to oracle";
+                                if (Status.Contains("Printed/Sent to oracle") || Status.Contains("Waiting to Interface"))
+                                {
+                                    excludeList.Add(new ExcludedOrder { WorkOrder = item.ID, Account = item.Account, Reason = Status });
+                                }
+                                else
+                                {
+                                    cancelList.Add(item.ID);
+                                }
+                            }
+                        }
                         if (System.IO.File.Exists(path))
                         {
                             // If file found, delete it    
                             System.IO.File.Delete(path);
                         }
-                        if (list != null && list.Any())
+                        if (cancelList != null && cancelList.Any())
                         {
                             var components = User.Identity.Name.Split('\\');
-                            var userName = components.Last();                           
+                            var userName = components.Last();
                             using (var _db = new USPS_Report.Models.ReportsEntities())
                             {
-
-                                _db.sp_CancelOrders(string.Join(",", list.Select(n => n.ToString()).ToArray()), Note, userName);
+                                _db.sp_CancelOrders(string.Join(",", cancelList.Select(n => n.ToString()).ToArray()), Note, userName);
+                                //_db.sp_CancelOrders(string.Join(",", list.Select(n => n.ToString()).ToArray()), Note, userName);
                                 string query = @"insert into Reports.dbo.tbl_ReportsAuditLine values('" + User.Identity.Name.Split('\\').Last().ToLower() + "',29,GETDATE())";
                                 int rowsinsert = _db.Database.ExecuteSqlCommand(query);
-                                return Json(new { Success = true, Message = "Successfully completed cancel orders" }, JsonRequestBehavior.AllowGet);
+                                return Json(new { Success = true, ExcludedData = excludeList, Message = "Successfully completed cancel orders" }, JsonRequestBehavior.AllowGet);
                             }
 
 
@@ -107,7 +134,7 @@ namespace USPS_Report.Areas.Reports.Controllers
                 else
                 {
                     //If file extension of the uploaded file is different then .xlsx  
-                     return Json(new { Success = false, Message = "Please select file with .xlsx extension!" }, JsonRequestBehavior.AllowGet);
+                    return Json(new { Success = false, Message = "Please select file with .xlsx extension!" }, JsonRequestBehavior.AllowGet);
                 }
 
                 return View();
@@ -116,9 +143,9 @@ namespace USPS_Report.Areas.Reports.Controllers
             {
                 throw;
             }
-            
+
         }
-       
+
 
         public ActionResult MassReleases()
         {
@@ -217,7 +244,7 @@ namespace USPS_Report.Areas.Reports.Controllers
                 throw;
             }
 
-            
+
         }
 
         public ActionResult UnCancelOrders()
@@ -280,10 +307,10 @@ namespace USPS_Report.Areas.Reports.Controllers
 
                             var components = User.Identity.Name.Split('\\');
                             var userName = components.Last();
-                             using (var _db = new USPS_Report.Models.ReportsEntities())
+                            using (var _db = new USPS_Report.Models.ReportsEntities())
                             {
-                             
-                                 _db.sp_UnCancelOrders(string.Join(",", list.Select(n => n.ToString()).ToArray()));
+
+                                _db.sp_UnCancelOrders(string.Join(",", list.Select(n => n.ToString()).ToArray()));
                                 string query = @"insert into Reports.dbo.tbl_ReportsAuditLine values('" + User.Identity.Name.Split('\\').Last().ToLower() + "',31,GETDATE())";
                                 int rowsinsert = _db.Database.ExecuteSqlCommand(query);
                                 return Json(new { Success = true, Message = "Successfully uncanceled orders" }, JsonRequestBehavior.AllowGet);
@@ -303,7 +330,7 @@ namespace USPS_Report.Areas.Reports.Controllers
                 else
                 {
                     //If file extension of the uploaded file is different then .xlsx  
-                   return Json(new { Success = false, Message = "Please select file with .xlsx extension!" }, JsonRequestBehavior.AllowGet);
+                    return Json(new { Success = false, Message = "Please select file with .xlsx extension!" }, JsonRequestBehavior.AllowGet);
                 }
 
                 return View();
---
+++ USPSReport/USPS_Report/Areas/Reports/Models/ManageOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USPS_Report.Areas.Reports.Models
+{
+    public class ManageOrder
+    {
+        public class CancelOrdersVM
+        {
+            public IList<ExcludedOrder> ExcludedOrders { get; set; }
+        }
+        public class ExcludedOrder
+        {
+            public int WorkOrder { get; set; }
+            public int? Account { get; set; }
+            public string Reason { get; set; }
+        }
+    }
+}
