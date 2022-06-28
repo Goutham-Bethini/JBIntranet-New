a6706de4c392f5eb21a1a430c0bc35cf46cca1f9
MaheswaraRaoK <55400944+MaheswaraRaoK@users.noreply.github.com>
2022-06-28 16:03:59 -0400

    Few more oracle changes.

--- USPSReport/USPS_Report/Areas/Reports/Models/QOH.cs
+++ USPSReport/USPS_Report/Areas/Reports/Models/QOH.cs
@@ -14,41 +14,15 @@ namespace USPS_Report.Areas.Reports.Models
 
         public static void getQOHOracle()
         {
-            string _conn = ConfigurationManager.ConnectionStrings["EntitiesOracle"].ConnectionString;
-            OleDbConnection myConnection = new OleDbConnection(_conn);
-            OleDbCommand cmd = new OleDbCommand();
-
-            DataSet ds = null;
-            OleDbDataAdapter adapter;
-
             try
             {
+                string _conn = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+                Oracle.ManagedDataAccess.Client.OracleConnection myConnection = new Oracle.ManagedDataAccess.Client.OracleConnection(_conn);
                 myConnection.Open();
-                ////Stored procedure calling. It is already in sample db.
-                cmd.CommandText = "proc=get_atr_quantity";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Connection = myConnection;
-
-                OleDbParameter paramReturnValue = new OleDbParameter();
-                paramReturnValue.ParameterName = "Qty_Available2";
-                paramReturnValue.OleDbType = OleDbType.Integer;
-                paramReturnValue.Direction = ParameterDirection.Output;
-
-                OleDbParameter paramA = new OleDbParameter();
-                paramA.ParameterName = "opqProductCode";
-                paramA.OleDbType = OleDbType.VarChar;
-                paramA.Direction = ParameterDirection.Input;
-                paramA.Value = "ONE550";
-
-                cmd.Parameters.Add(paramReturnValue);
-                cmd.Parameters.Add(paramA);
-
-                cmd.ExecuteNonQuery();
-
-                int returnValue = (int)cmd.Parameters["Qty_Available2"].Value;
-
-              //  Output.Items.Add(returnValue);
-
+                string Query = @"select Available_qty from XXJBM_ITEM_ONHAND_QUANTITY where HDMS_ITEM ='ONE550'";
+                int availableQty = 0;
+                Oracle.ManagedDataAccess.Client.OracleCommand cmd = new Oracle.ManagedDataAccess.Client.OracleCommand(Query, myConnection);
+                availableQty = cmd.ExecuteScalar() != null ? string.IsNullOrEmpty(cmd.ExecuteScalar().ToString()) ? 0 : (int)cmd.ExecuteScalar() : 0;
                 myConnection.Close();
             }
             catch (Exception ex)
@@ -56,7 +30,45 @@ namespace USPS_Report.Areas.Reports.Models
                 throw new Exception(ex.Message);
             }
 
+            //DataSet ds = null;
+            //OleDbDataAdapter adapter;
+
+            //try
+            //{
+            //    myConnection.Open();
+            //    ////Stored procedure calling. It is already in sample db.
+            //    cmd.CommandText = "proc=get_atr_quantity";
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    cmd.Connection = myConnection;
+
+            //    OleDbParameter paramReturnValue = new OleDbParameter();
+            //    paramReturnValue.ParameterName = "Qty_Available2";
+            //    paramReturnValue.OleDbType = OleDbType.Integer;
+            //    paramReturnValue.Direction = ParameterDirection.Output;
+
+            //    OleDbParameter paramA = new OleDbParameter();
+            //    paramA.ParameterName = "opqProductCode";
+            //    paramA.OleDbType = OleDbType.VarChar;
+            //    paramA.Direction = ParameterDirection.Input;
+            //    paramA.Value = "ONE550";
+
+            //    cmd.Parameters.Add(paramReturnValue);
+            //    cmd.Parameters.Add(paramA);
+
+            //    cmd.ExecuteNonQuery();
+
+            //    int returnValue = (int)cmd.Parameters["Qty_Available2"].Value;
+
+            //  //  Output.Items.Add(returnValue);
+
+            //    myConnection.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw new Exception(ex.Message);
+            //}
+
         }
-       
+
     }
 }
