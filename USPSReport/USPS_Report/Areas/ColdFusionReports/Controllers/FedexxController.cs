b95e6b1816d08e18b7e0605e5c27df313fa1b04d
VishnuVardhan-JB <116829952+VishnuVardhan-JB@users.noreply.github.com>
2023-05-30 05:55:40 -0400

    ticket 10-03012023 fix

---
+++ USPSReport/USPS_Report/Areas/ColdFusionReports/Controllers/FedexxController.cs
@@ -0,0 +1,52 @@
+﻿using ReportsDatabase;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using USPS_Report.Areas.ColdFusionReports.Models;
+using USPS_Report.Areas.ColdFusionReports.Models.DataModels;
+
+namespace USPS_Report.Areas.ColdFusionReports.Controllers
+{
+    public class FedexxController : Controller
+    {
+        // GET: ColdFusionReports/Fedexx
+        public ActionResult Index(int? workOrderId)
+        {
+           
+            
+            List<WorkOrdersModel> list = new List<WorkOrdersModel>();
+
+            if (workOrderId != null)
+            {
+                try
+                {
+                    string OraConnection = ConfigurationManager.ConnectionStrings["EntitiesOracle1"].ConnectionString;
+                    string Query = @"select * from tbl_ups_workorders where id_workorder ='" + workOrderId + "'";
+                    
+                    using (OleDbConnection conn = new OleDbConnection(OraConnection))
+                    {
+                        conn.Open();
+                        using (OleDbCommand cmd = new OleDbCommand(Query, conn))
+                        {
+                            list = (List<WorkOrdersModel>)cmd.ExecuteScalar();
+                        }
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+
+            return View();
+        }
+
+      
+    }
+}
---
+++ USPSReport/USPS_Report/Areas/ColdFusionReports/Models/WorkOrderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USPS_Report.Areas.ColdFusionReports.Models
+{
+    public class WorkOrderModel
+    {
+
+        public string WorkOrder { get; set; }
+        public string DateShipped { get; set; }
+        public string PackageWeight { get; set; }
+        public string PackedBy { get; set; }
+        public string ConfirmationNumber { get; set; }
+
+    }
+}
